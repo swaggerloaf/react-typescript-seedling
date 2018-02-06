27f3cd8c0194fbb7c037555530dde1cd159b726f
swaggerloaf <swaggerloaf@gmail.com>
2018-02-05 22:48:47 -0500

    Updates

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-var pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
-var mappedPizzas = pizzas.map(function (pizza) {
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }, { name: 'Extra cheeze', toppings: ['pepperoni'] }];
+// tslint:disable-next-line:typedef
+const mappedPizzas = pizzas.map(function (pizza) {
     return pizza.name.toUpperCase();
 });
 console.log(mappedPizzas);
